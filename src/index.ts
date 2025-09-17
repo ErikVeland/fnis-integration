8290dd6846d168e3f31e4de48d4e8b8eae98320b
Erik Veland <erikveland@gmail.com>
2025-09-17 15:48:58 +1000

    chore(extensions/fnis-integration): sweep and commit untracked changes
     M src/actions.ts
     M src/fnis.ts
     M src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -38,11 +38,11 @@ function checkFailedResult(t: TranslationFunction,
     missing: t('You have configured Vortex to run FNIS automatically but it\'s not installed for this game. '
               + 'For the automation to work, FNIS has to be installed and configured for the current game. '
               + 'You can download it from [url]{{url}}[/url].',
-              { replace: { url: nexusPageURL(gameMode) } }),
+               { replace: { url: nexusPageURL(gameMode) } }),
     outdated: t('You have configured Vortex to run FNIS automatically but the installed version of FNIS is '
                 + 'too old and doesn\'t support being embedded. Please download and install at least '
                 + 'version 7.4 from [url]{{url}}[/url].',
-              { replace: { url: nexusPageURL(gameMode) } }),
+                { replace: { url: nexusPageURL(gameMode) } }),
   }[reason];
 
   const res: types.ITestResult = {
@@ -90,38 +90,38 @@ function init(context: types.IExtensionContext) {
       util.getSafe(state, ['settings', 'fnis', 'patches', profile.id], []));
 
     readFNISPatches(context.api, profile)
-    .then(patches => {
-      return context.api.showDialog('question', 'Select patches for this profile', {
-        text: 'Please select the patches to activate in FNIS (when run automatically!).\n'
+      .then(patches => {
+        return context.api.showDialog('question', 'Select patches for this profile', {
+          text: 'Please select the patches to activate in FNIS (when run automatically!).\n'
             + 'Only select patches you have the corresponding mod for!\n'
             + 'This list is stored separately for each profile.',
-        checkboxes: patches.map(patch => ({
-          id: patch.id,
-          text: patch.description,
-          value: enabledPatches.has(patch.id),
-        })),
-      }, [
-        { label: 'Cancel' },
-        { label: 'Save' },
-      ]);
-    })
-    .then((result: types.IDialogResult) => {
-      if (result.action === 'Save') {
-        context.api.store.dispatch(setPatches(profile.id,
-          Object.keys(result.input).filter(patch => result.input[patch])));
-        context.api.store.dispatch(setNeedToRun(profile.id, true));
-      }
-    })
-    .catch(err => {
-      const userCanceled = (err instanceof util.UserCanceled);
-      if ((err instanceof util.ProcessCanceled) || userCanceled) {
-        if (userCanceled) {
-          log('debug', 'Failed to read list of available patches', err);
+          checkboxes: patches.map(patch => ({
+            id: patch.id,
+            text: patch.description,
+            value: enabledPatches.has(patch.id),
+          })),
+        }, [
+          { label: 'Cancel' },
+          { label: 'Save' },
+        ]);
+      })
+      .then((result: types.IDialogResult) => {
+        if (result.action === 'Save') {
+          context.api.store.dispatch(setPatches(profile.id,
+                                                Object.keys(result.input).filter(patch => result.input[patch])));
+          context.api.store.dispatch(setNeedToRun(profile.id, true));
         }
-        return;
-      }
-      context.api.showErrorNotification('Failed to read list of available patches', err);
-    });
+      })
+      .catch(err => {
+        const userCanceled = (err instanceof util.UserCanceled);
+        if ((err instanceof util.ProcessCanceled) || userCanceled) {
+          if (userCanceled) {
+            log('debug', 'Failed to read list of available patches', err);
+          }
+          return;
+        }
+        context.api.showErrorNotification('Failed to read list of available patches', err);
+      });
   }, () => {
     const state = context.api.store.getState();
     const gameMode = selectors.activeGameId(state);
@@ -204,84 +204,84 @@ function init(context: types.IExtensionContext) {
       }
     });
     (context.api as any).onAsync('did-deploy',
-        (profileId: string, deployment: IDeployment, setTitle: (title: string) => void) => {
-      const { store } = context.api;
-      const state = context.api.getState();
-      if (lastChecksum !== undefined) {
-        const profile = state.persistent.profiles[profileId];
-        if (profile === undefined) {
+                                 (profileId: string, deployment: IDeployment, setTitle: (title: string) => void) => {
+                                   const { store } = context.api;
+                                   const state = context.api.getState();
+                                   if (lastChecksum !== undefined) {
+                                     const profile = state.persistent.profiles[profileId];
+                                     if (profile === undefined) {
           // profile got deleted while deploying? Shouldn't have been possible
-          return Promise.resolve();
-        }
-        const discovery: types.IDiscoveryResult =
+                                       return Promise.resolve();
+                                     }
+                                     const discovery: types.IDiscoveryResult =
           state.settings.gameMode.discovered[profile.gameId];
 
-        if ((discovery === undefined) || (discovery.path === undefined)) {
+                                     if ((discovery === undefined) || (discovery.path === undefined)) {
           // game got "undiscovered"?
-          return Promise.resolve();
-        }
+                                       return Promise.resolve();
+                                     }
 
-        const modId = fnisDataMod(profile.name);
-        const allMods = state.persistent.mods[profile.gameId] || {};
+                                     const modId = fnisDataMod(profile.name);
+                                     const allMods = state.persistent.mods[profile.gameId] || {};
         // TODO: this is a hack. We don't want the FNIS Data mod being enabled to trigger a new
         //   deployment, but this is probably true for everything that runs as a post-deploy
         //   callback but _not_ for everything else that is triggered separately
-        const didNeedDeployment = state.persistent.deployment.needToDeploy[profile.gameId];
-        let dependentMods: string[];
-        return calcChecksum(discovery.path, deployment)
-          .then(({ checksum, mods }) => {
-            dependentMods = mods;
-            log('debug', 'Animations checksum after deployment', checksum);
-            if (!didNeedDeployment) {
-              store.dispatch(actions.setDeploymentNecessary(profile.gameId, false));
-            }
-            if ((checksum === lastChecksum)
+                                     const didNeedDeployment = state.persistent.deployment.needToDeploy[profile.gameId];
+                                     let dependentMods: string[];
+                                     return calcChecksum(discovery.path, deployment)
+                                       .then(({ checksum, mods }) => {
+                                         dependentMods = mods;
+                                         log('debug', 'Animations checksum after deployment', checksum);
+                                         if (!didNeedDeployment) {
+                                           store.dispatch(actions.setDeploymentNecessary(profile.gameId, false));
+                                         }
+                                         if ((checksum === lastChecksum)
                 && (allMods[modId] !== undefined)
                 && (allMods[modId].installationPath !== undefined)
                 && !util.getSafe(state, ['settings', 'fnis', 'needToRun', profile.id], false)) {
-              return Promise.resolve();
-            }
+                                           return Promise.resolve();
+                                         }
 
-            setTitle(context.api.translate('Updating animations (FNIS)'));
-            return Promise.resolve(fnis(context.api, profile, false));
-          })
-          .then(() => {
-            store.dispatch(actions.clearModRules(profile.gameId, modId));
-            dependentMods.forEach(refId => {
-              log('debug', 'add fnis data load-after', { refId });
-              if (refId === modId) {
-                return;
-              }
-              store.dispatch(actions.addModRule(profile.gameId, modId, {
-                type: 'after',
-                reference: { id: refId },
-              }));
-            });
+                                         setTitle(context.api.translate('Updating animations (FNIS)'));
+                                         return Promise.resolve(fnis(context.api, profile, false));
+                                       })
+                                       .then(() => {
+                                         store.dispatch(actions.clearModRules(profile.gameId, modId));
+                                         dependentMods.forEach(refId => {
+                                           log('debug', 'add fnis data load-after', { refId });
+                                           if (refId === modId) {
+                                             return;
+                                           }
+                                           store.dispatch(actions.addModRule(profile.gameId, modId, {
+                                             type: 'after',
+                                             reference: { id: refId },
+                                           }));
+                                         });
 
-            store.dispatch(actions.setModEnabled(profile.id, modId, true));
-            store.dispatch(setNeedToRun(profile.id, false));
-            return context.api.emitAndAwait('deploy-single-mod', profile.gameId, modId);
-          })
-          .catch(err => {
-            if (err instanceof util.UserCanceled) {
-              return Promise.resolve();
-            }
-            if (err.code === 'UNKNOWN') {
-              context.api.showErrorNotification('Failed to run FNIS',
-                'An unknown error occurred starting FNIS. This is usually caused by a broken '
+                                         store.dispatch(actions.setModEnabled(profile.id, modId, true));
+                                         store.dispatch(setNeedToRun(profile.id, false));
+                                         return context.api.emitAndAwait('deploy-single-mod', profile.gameId, modId);
+                                       })
+                                       .catch(err => {
+                                         if (err instanceof util.UserCanceled) {
+                                           return Promise.resolve();
+                                         }
+                                         if (err.code === 'UNKNOWN') {
+                                           context.api.showErrorNotification('Failed to run FNIS',
+                                                                             'An unknown error occurred starting FNIS. This is usually caused by a broken '
                 + 'installation or the tool being set up incorrectly.',
-                { allowReport: false, id: 'fnis-failed-to-run' });
-            } else {
-              const isMisconfigured = (err instanceof util.SetupError);
-              context.api.showErrorNotification('Failed to run FNIS',
-                isMisconfigured ? 'Please install FNIS and add it as a tool inside Vortex' : err,
-                { allowReport: !isMisconfigured, id: 'fnis-failed-to-run' });
-            }
-          });
-      } else {
-        return Promise.resolve();
-      }
-    });
+                                                                             { allowReport: false, id: 'fnis-failed-to-run' });
+                                         } else {
+                                           const isMisconfigured = (err instanceof util.SetupError);
+                                           context.api.showErrorNotification('Failed to run FNIS',
+                                                                             isMisconfigured ? 'Please install FNIS and add it as a tool inside Vortex' : err,
+                                                                             { allowReport: !isMisconfigured, id: 'fnis-failed-to-run' });
+                                         }
+                                       });
+                                   } else {
+                                     return Promise.resolve();
+                                   }
+                                 });
   });
 }
 
